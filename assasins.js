2f10d0a926fe7aa6d76ebb6f02b735beedfbd59a
shakhzodbekFLST <ismatovshahzodbek0@gmail.com>
2021-12-10 21:42:29 +0500

    initial commit

---
+++ assasins.js
@@ -0,0 +1,9 @@
+window.addEventListener('DOMContentLoaded',()=>{
+   const Btn=document.querySelector('.send');
+
+   Btn.addEventListener('click',(event)=>{
+       event.preventDefault()
+   })
+    
+
+})
